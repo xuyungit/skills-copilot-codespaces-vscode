0e00048c596cd79f365eaf493323fc3d3d89fc13
xuyungit <xu_yy@live.com>
2023-12-17 12:27:32 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberSkills = document.getElementById("memberSkills");
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberSkills.style.display = "none";
+}
